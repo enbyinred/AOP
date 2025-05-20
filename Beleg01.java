0e4f4c7992dd5923b9945392fc73a464df145180
enbyinred <sommer20j@gmail.com>
2025-05-20 13:36:51 +0200

    AOP Beleg01 Grundgerüst

---
+++ Beleg01.java
@@ -0,0 +1,19 @@
+package AOP_Beleg01;
+
+import java.util.Scanner;
+
+public class Beleg01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Geben Sie einen Satz ein: ");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String eingabe = scanner.nextLine();
+		
+		System.out.println(eingabe);
+		
+	}
+
+}
