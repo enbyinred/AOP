7a48b2e05e07589247bf13c34692d2941903340b
enbyinred <sommer20j@gmail.com>
2025-05-23 14:53:20 +0200

    erste ansätze zur loop

--- Beleg01.java
+++ Beleg01.java
@@ -6,17 +6,44 @@ public class Beleg01 {
 
 	public static void main(String[] args) {
 		
+		
+		int i = 0; int j = 0; int k= 0;
+		
+		private final static String vokale = "aeiouAEIOU";
+			
+
+		
+		
+	
+		
 		System.out.println("Geben Sie einen Satz ein: ");
 		
 		Scanner scanner = new Scanner(System.in);
 		
 		String eingabe = scanner.nextLine();
-		
+	
+
 		if (eingabe.length() <= 80) {
+			
+			public static String vokaleEntf(String string) {
+				
+				if (eingabe.charAt(i)!= vokale.charAt(j)) {
+					
+					k+= eingabe.charAt(i);
+				
+				return string;
+				}
+				
+				}
+				
+			}
+			
+		
 			
 			System.out.println(eingabe);
 			
 		}
+		
 		else {
 			
 			System.out.println("ERROR");
